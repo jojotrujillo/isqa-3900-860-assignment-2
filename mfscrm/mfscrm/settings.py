c8db7d3cec935f401e67c4ecfe900add8bf8178b
Jojo Trujillo <56196564+jojotrujillo@users.noreply.github.com>
2020-02-26 15:49:54 -0600

    Revert "Update settings.py"
    This reverts commit 58a598370743052001dee34eda4b172d625a11f5.

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u^lxqcz7#!*t7g!1f$y%l-($^32@+bwr1)n=vu9ygez)2xy7yo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
